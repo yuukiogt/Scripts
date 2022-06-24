c019dd9e4f272d95cce6eb0f0ebc70a74aec2bc6
yuukiogt <penguinko91@gmail.com>
2022-06-24 11:56:12 +0900

    add Set-ServiceFailureSettings

---
+++ Set-ServiceFailureSettings.ps1
@@ -0,0 +1,29 @@
+﻿$resetTime = 600
+$restartSpan = 180000
+
+while(1) {
+    $computerName = Read-Host "computerName or q(quit)"
+    if ($computerName -eq 'q') {
+        exit
+    }
+
+    if (-not (Test-Connection $computerName -Count 1)) {
+        continue
+    }
+    
+    while(1) {
+        $serviceName = Read-Host "serviceName or q(quit)"
+        if ($serviceName -eq 'q') {
+            break
+        }
+
+        $service = Get-Service $serviceName
+        if ($Null -eq $service) {
+            continue
+        }
+
+        $target = "\\$($computerName)"
+
+        sc.exe $target failure $serviceName reset=$resetTime actions=restart/$restartSpan
+    }
+}
